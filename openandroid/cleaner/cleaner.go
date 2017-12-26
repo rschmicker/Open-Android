841d6c04a012ffe3f78a9c2bfb0b1e60b19bc152
Rob Schmicker <rhschmicker@gmail.com>
2017-12-26 15:24:39 -0500

    multi thread

--- openandroid/cleaner/cleaner.go
+++ openandroid/cleaner/cleaner.go
@@ -7,12 +7,19 @@ import (
 	"log"
 	"os"
 	"strings"
+	"sync"
 )
 
 func CleanDirectory(config utils.ConfigData) {
+	var wg sync.WaitGroup
+	sem := make(chan struct{}, 1000)
 	fileList := cache.GetPaths(config.ApkDir, ".apk")
 	for _, file := range fileList {
+		wg.Add(1)
 		go func(file string) {
+			sem <- struct{}{}
+			defer func() { <-sem }()
+			defer wg.Done()
 			hash := metadata.Sha256File(file)
 			newPath := ""
 			if strings.Contains(file, "benign") {
