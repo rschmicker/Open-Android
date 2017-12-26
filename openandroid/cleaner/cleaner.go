0590c8b0cf3d6a4a44ae6bcdc57be256c42dcf6a
Rob Schmicker <rhschmicker@gmail.com>
2017-12-26 15:15:13 -0500

    multi thread

--- openandroid/cleaner/cleaner.go
+++ openandroid/cleaner/cleaner.go
@@ -12,18 +12,21 @@ import (
 func CleanDirectory(config utils.ConfigData) {
 	fileList := cache.GetPaths(config.ApkDir, ".apk")
 	for _, file := range fileList {
-		hash := metadata.Sha256File(file)
-		newPath := ""
-		if strings.Contains(file, "benign") {
-			newPath = config.ApkDir + "/benign/" + hash + ".apk"
-		} else {
-			newPath = config.ApkDir + "/malware/" + hash + ".apk"
-		}
-		if file == newPath {
-			continue
-		}
-		err := os.Rename(file, newPath)
-		utils.Check(err)
-		log.Printf("Cleaned: %v -> %v", file, newPath)
+		go func(file string) {
+			hash := metadata.Sha256File(file)
+			newPath := ""
+			if strings.Contains(file, "benign") {
+				newPath = config.ApkDir + "/benign/" + hash + ".apk"
+			} else {
+				newPath = config.ApkDir + "/malware/" + hash + ".apk"
+			}
+			if file == newPath {
+				log.Printf("Skipping: %v already cleaned", file)
+				continue
+			}
+			err := os.Rename(file, newPath)
+			utils.Check(err)
+			log.Printf("Cleaned: %v -> %v", file, newPath)
+		}(file)
 	}
 }
