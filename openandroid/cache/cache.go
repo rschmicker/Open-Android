c82f19b9a5b768350f3c3339e405f3d8fbbbfd27
Rob Schmicker <rhschmicker@gmail.com>
2017-12-28 16:19:50 -0500

    Remove duplicates

--- openandroid/cache/cache.go
+++ openandroid/cache/cache.go
@@ -43,6 +43,7 @@ func (ct *CacheTable) Initialize(config utils.ConfigData) int {
 		doneFiles := GetPaths(config.OutputDir, ".json")
 		ct.Files = CrossCompare(toDoFiles, doneFiles)
 	}
+	RemoveDuplicates(&ct.Files)
 	if len(ct.Files) == 0 {
 		log.Fatal("No Files found")
 	}
@@ -162,6 +163,19 @@ func (ct *CacheTable) RemoveFileFromList(filePath string) {
 func (ct *CacheTable) Close() {
 	err := os.RemoveAll(ct.RamDiskPath)
 	utils.Check(err)
+}
+
+func RemoveDuplicates(xs *[]string) {
+	found := make(map[string]bool)
+	j := 0
+	for i, x := range *xs {
+		if !found[x] {
+			found[x] = true
+			(*xs)[j] = (*xs)[i]
+			j++
+		}
+	}
+	*xs = (*xs)[:j]
 }
 
 func CrossCompare(todoFiles []string, doneFiles []string) []string {
