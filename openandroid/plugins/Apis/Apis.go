248b4f8b6ef4017dd24d93217d3b166ffacf2573
Rob Schmicker <rhschmicker@gmail.com>
2018-01-31 17:52:24 -0500

    api prefix fix

--- openandroid/plugins/Apis/Apis.go
+++ openandroid/plugins/Apis/Apis.go
@@ -36,10 +36,15 @@ func GetValue(path string, config utils.ConfigData) (interface{}, error) {
 	data := strings.Split(out.String(), "\n")
 	data = data[5:]
 	for i := 0; i < len(data)-1; i++ {
+		previous := false
 		tmp := strings.Replace(data[i], "\t", "", -1)
 		for _, c := range tmp {
+			if !unicode.IsDigit(c) && previous {
+				break
+			}
 			if unicode.IsDigit(c) {
 				tmp = tmp[1:]
+				previous = true
 			}
 		}
 		data[i] = tmp
