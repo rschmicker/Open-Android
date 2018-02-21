c54b68e26e1cb9882cf36a83a3e40934eb769797
rschmicker <rhschmicker@gmail.com>
2018-02-20 20:23:13 -0500

    backup

--- webserver/api/api.go
+++ webserver/api/api.go
@@ -4,30 +4,90 @@ import(
 	"github.com/olivere/elastic"
 	"net/http"
 	"golang.org/x/net/context"
-	"golang.org/x/sync/errgroup"
 	"encoding/json"
 	"io"
-	"bytes"
-	//"log"
+	"strings"
+	"github.com/Open-Android/webserver/utils"
+	"log"
 )
 
-type ApkData struct {
-	Permissions		[]string	`json:"Permissions"`
-	Malicious		string		`json:"Malicious"`
-}
-
 func Query(w http.ResponseWriter, req *http.Request) {
+	w.Header().Add("Strict-Transport-Security", "max-age=63072000; includeSubDomains")
 	client, err := elastic.NewClient()
 	if err != nil {
 		panic(err)
 	}
 	ctx := context.Background()
-	// Count total and setup progress
-	// total, err := client.Count("apks").Type("apk").Do(ctx)
-	// if err != nil {
-	// 	panic(err)
-	// }
-	//bar := pb.StartNew(int(total))
+
+	args := req.URL.Query()
+        toArg, _ := utils.GetArg("to", args)
+        fromArg, _ := utils.GetArg("from", args)
+	flds, _ := utils.GetArg("fields", args)
+        //http.Error(w, InfoMsg, http.StatusBadRequest)
+
+	fields := []string{}
+	if len(flds) > 0 {
+		fields = strings.Split(flds, ",")
+	}
+
+	rangeQuery := elastic.NewRangeQuery("Date")
+	if len(fromArg) == 0 {
+		rangeQuery = rangeQuery.From(nil)
+	} else {
+		rangeQuery = rangeQuery.From(fromArg)
+	}
+	if len(toArg) == 0 {
+		rangeQuery = rangeQuery.To(nil)
+	} else {
+		rangeQuery = rangeQuery.To(toArg)
+	}
+
+	log.Println("===============================")
+	log.Println("From: " + fromArg)
+	log.Println("To: " + toArg)
+	log.Println("Fields: ")
+	log.Println(fields)
+	log.Println("===============================")
+
+	scroll := client.Scroll("apks").Type("apk").Query(rangeQuery).Size(100)
+	io.WriteString(w, "{\"data\":[")
+	for {
+		results, err := scroll.Do(ctx)
+		if err == io.EOF {
+			break// all results retrieved
+		}
+		if err != nil {
+			log.Println(err.Error())
+			break
+		}
+			// Send the hits to the hits channel
+		for _, hit := range results.Hits.Hits {
+			if len(fields) == 0 {
+				w.Write(*hit.Source)
+				io.WriteString(w, ",")
+				continue
+			}
+			data := make(map[string]interface{})
+                        out := make(map[string]interface{})
+			err := json.Unmarshal(*hit.Source, &data)
+                        if err != nil {
+				panic(err)
+                        }
+			for key, val := range data {
+				if utils.StringInSlice(key, fields) {
+					out[key] = val
+				}
+			}
+			writer := []byte{}
+                        writer, err = json.Marshal(out)
+                        if err != nil {
+				panic(err)
+                        }
+                        w.Write(writer)
+			io.WriteString(w, ",")
+		}
+	}
+	io.WriteString(w, "]}")
 
 	// This example illustrates how to use goroutines to iterate
 	// through a result set via ScrollService.
@@ -46,7 +106,7 @@ func Query(w http.ResponseWriter, req *http.Request) {
 	// Let's go.
 
 	// 1st goroutine sends individual hits to channel.
-	hits := make(chan json.RawMessage)
+	/*hits := make(chan json.RawMessage)
 	g, ctx := errgroup.WithContext(ctx)
 	g.Go(func() error {
 		defer close(hits)
@@ -76,27 +136,20 @@ func Query(w http.ResponseWriter, req *http.Request) {
 	// 2nd goroutine receives hits and deserializes them.
 	//
 	// If you want, setup a number of goroutines handling deserialization in parallel.
-	apks := make(chan json.RawMessage)
 	for i := 0; i < 10; i++ {
 		g.Go(func() error {
 			for hit := range hits {
-				//r := bytes.NewReader(hit)
-				apks <- hit
-				//io.Copy(w, r)
-				// // Deserialize
-				// var a ApkData
-				// err := json.Unmarshal(hit, &a)
-				// if err != nil {
-				// 	return err
-				// }
-
-				// // Do something with the product here, e.g. send it to another channel
-				// // for further processing.
-				// _ = a
-
-				//bar.Increment()
-
-				// Terminate early?
+				data := &ApkData{}
+				err := json.Unmarshal(hit, &data)
+				if err != nil {
+					return err
+				}
+				writer := []byte{}
+				writer, err = json.Marshal(data)
+				if err != nil {
+					return err
+				}
+				w.Write(writer)
 				select {
 				default:
 				case <-ctx.Done():
@@ -107,30 +160,8 @@ func Query(w http.ResponseWriter, req *http.Request) {
 		})
 	}
 
-	g.Go(func() error {
-		for a := range apks {
-			data := &ApkData{}
-			err := json.Unmarshal(a, &data)
-			if err != nil {
-				panic(err)
-			}
-			writer := []byte{}
-			writer, err = json.Marshal(data)
-			if err != nil {
-				panic(err)
-			}
-			r := bytes.NewReader(writer)
-			io.Copy(w, r)
-		}
-		return nil
-	})
-
 	// Check whether any goroutines failed.
 	if err := g.Wait(); err != nil {
 		panic(err)
-	}
-
-	// Done.
-	//bar.FinishPrint("Done")
-	//return nil
+	}*/
 }
--- webserver/main.go
+++ webserver/main.go
@@ -3,180 +3,21 @@ package main
 import (
 	"crypto/tls"
 	"flag"
-	"fmt"
-	"io"
-	"io/ioutil"
 	"log"
 	"net/http"
-	"os"
-	//"strings"
+	"github.com/Open-Android/webserver/utils"
 	"github.com/Open-Android/webserver/api"
 )
-
-const InfoMsg string = `
-No GET parameters found!
-
-Here's some brief info on how to query:
-
-q=
-     - Query search parameter (Required)
-     - If you'd like to download everything, use q=*:*
-     - Search using regex or for a particular text with q=<field name>:<regex or literal>
-     - Example: q=Malicious:true
-        - Returns all APKs that are malicious
-
-fl=
-     - Return only these fields
-     - Example: fl=Permissions+Apis+PackageName
-        - Returns only the fields Permissions, Apis, and PackageName
-
-omitHeader=
-     - Remove extra query info
-     - Example: omitHeader=true
-
-rows=
-     - Number of entries to return
-     - Example: rows=7000
-         - Returns top 7000 entries of given query
-
-wt=
-     - Writer Type
-     - Type of file that is returned from the query
-     - json, csv, xml, etc.
-     - Default of json
-     - Example: wt=json
-
-More detailed descriptions and examples can be seen on our wiki: https://github.com/rschmicker/Open-Android/wiki
-
-`
-
-func getArg(name string, aMap map[string][]string) (arg string, err error) {
-	var ok bool
-	var arglist []string
-	if arglist, ok = aMap[name]; !ok {
-		return "", fmt.Errorf("unable to obtain arg from map with key %s", name)
-	}
-	return arglist[0], nil
-}
-
-func getSolrQuery(url string) string {
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		log.Println("Warning: " + err.Error())
-	}
-	res, err := http.DefaultClient.Do(req)
-	if err != nil {
-		log.Println("Warning: " + err.Error())
-	}
-	defer res.Body.Close()
-
-	body, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		log.Println("Warning: " + err.Error())
-	}
-	return string(body)
-}
-
-// func ArffGenerator(w http.ResponseWriter, req *http.Request) {
-// 	w.Header().Add("Strict-Transport-Security", "max-age=63072000; includeSubDomains")
-// 	args := req.URL.Query()
-// 	q, err := getArg("q", args)
-// 	if err != nil {
-// 		log.Println("Warning: " + err.Error())
-// 		http.Error(w, InfoMsg, http.StatusBadRequest)
-// 		return
-// 	}
-// 	// migrate this to python file, call python file "plugin" and send back response
-// 	// Include date range
-// 	solrQuery := "http://localhost:8983/solr/apks/select?q=*:*&fl="
-// 	if q == "Permissions" {
-// 		solrQuery += "Malicious," + q
-// 	} else {
-// 		http.Error(w, InfoMsg, http.StatusBadRequest)
-// 		return
-// 	}
-// 	solrQuery += "&wt=csv"
-// 	data := getSolrQuery(solrQuery)
-// 	solrResponse := strings.Split(data, "\n")
-// 	io.WriteString(w, "@relation '" + q + "'\n")
-// 	permissions := io.ReadFile("./permissions.txt")
-// 	io.WriteString(w, "@attribute Malicious numeric")
-// 	for _, p := range permissions {
-// 		io.WriteString(w, "@attribute " + attr + " numeric\n")
-// 	}
-// 	io.WriteString(w, "@data\n")
-// 	for i := 1; i < len(solrResponse); i++ {
-// 		line := solrResponse[i]
-// 		entries := strings.Split(line, ",")
-// 		if entries[0] == "true" {
-// 			io.WriteString(w, "1,")
-// 		} else {
-// 			io.WriteString(w, "0,")
-// 		}
-// 		for _, sp := range entries[1:] {
-// 				for _, p := range permissions {
-// 					if sp == p {
-						
-// 					}
-// 				}
-// 		}
-// 	}
-// }
-
-func Query(w http.ResponseWriter, req *http.Request) {
-	w.Header().Add("Strict-Transport-Security", "max-age=63072000; includeSubDomains")
-	args := req.URL.Query()
-	q, err := getArg("q", args)
-	if err != nil {
-		log.Println("Warning: " + err.Error())
-		http.Error(w, InfoMsg, http.StatusBadRequest)
-		return
-	}
-	fl, _ := getArg("fl", args)
-	omitHeader, _ := getArg("omitHeader", args)
-	rows, _ := getArg("rows", args)
-	wt, _ := getArg("wt", args)
-
-	solrQuery := "http://localhost:8983/solr/apks/select?"
-	solrQuery += "q=" + q
-	if len(fl) != 0 {
-		solrQuery += "&fl=" + fl
-	}
-	if len(omitHeader) != 0 {
-		solrQuery += "&omitHeader=" + omitHeader
-	}
-	if len(rows) != 0 {
-		solrQuery += "&rows=" + rows
-	}
-	if len(wt) != 0 {
-		solrQuery += "&wt=" + wt
-	} else {
-		solrQuery += "&wt=json"
-	}
-	io.WriteString(w, getSolrQuery(solrQuery))
-}
-
-func PrintUsage() {
-	fmt.Println(`
-Syntax:
-	>webserver -key <Directory to HTTPS key> -cert <Directory to HTTPS certificate>
-
-Example:
-	>webserver -key ./keys/server.key -cert ./keys/server.crt
-`)
-	os.Exit(1)
-}
 
 func main() {
 	keyFlag := flag.String("key", "", "Location to HTTPS key.")
 	certFlag := flag.String("cert", "", "Location to HTTPS certificate.")
 	flag.Parse()
 	if (len(*keyFlag) == 0) || (len(*certFlag) == 0) {
-		PrintUsage()
+		utils.PrintUsage()
 	}
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", api.Query)
-	//mux.HandleFunc("/arff", ArffGenerator)
 	cfg := &tls.Config{
 		MinVersion:               tls.VersionTLS12,
 		CurvePreferences:         []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
