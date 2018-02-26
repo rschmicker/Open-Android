ca68f4bddfeed1e6a45faa4a51b5eef475b50cee
Rob Schmicker <rhschmicker@gmail.com>
2018-02-26 13:55:07 -0500

    implemented models

--- analysis/train_oa.py
+++ analysis/train_oa.py
@@ -12,6 +12,8 @@ from sklearn import linear_model
 from sklearn.naive_bayes import GaussianNB
 from sklearn.cluster import KMeans
 from sklearn.model_selection import KFold
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
 
 # dl: 50 minutes for perms, filesize, and malicious for 94,000 apks, 12.9KB/s
 def get_apk_json(filepath):
@@ -28,42 +30,48 @@ def get_permissions(apk):
 	return perms
 
 def main():
-	#model = linear_model.LogisticRegression()
-	model = svm.SVC()
-	#model = GaussianNB()
-	#model = KMeans(n_clusters = 10)
+	models = {}
+	models["SimpleLogistic"] = linear_model.LogisticRegression() # simple logistic
+	models["SMO"] = svm.SVC() # SMO
+	models["NaiveBayes"] = GaussianNB() # naive bayes
+	models["RandomTree"] = DecisionTreeClassifier(random_state=0) # random tree
+	models["RandomForest10"] = RandomForestClassifier(max_depth=10, random_state=0) # random forest, max depth: 10
+	models["RandomForest50"] = RandomForestClassifier(max_depth=50, random_state=0) # random forest, max depth: 50
+	models["RandomForest100"] = RandomForestClassifier(max_depth=100, random_state=0) # random forest, max depth: 100
+
 	apks = get_apk_json("permsfilesize.json")
 	feature_vector = []
 	target_vector = []
 	apks = apks['data']
-	kf = KFold(10, True, None)
-	kf.get_n_splits(apks)
-	train_apks = []
-	predict_apks = []
-	for train, test in kf.split(apks):
-		train_apks = train
-		predict_apks = test
-		break
 
-	print("training...")
-	for idx in train_apks:
-		feature_vector.append(get_permissions(apks[idx]))
-		target_type = 1 if apks[idx]['Malicious'] == 'true' else 0
-		target_vector.append(target_type)
-	clf = model
-	clf.fit(feature_vector, target_vector)
+	for model_name, model in models:
+		kf = KFold(10, True, None)
+		kf.get_n_splits(apks)
+		train_apks = []
+		predict_apks = []
+		for train, test in kf.split(apks):
+			train_apks = train
+			predict_apks = test
+			break
+		print("training...")
+		for idx in train_apks:
+			feature_vector.append(get_permissions(apks[idx]))
+			target_type = 1 if apks[idx]['Malicious'] == 'true' else 0
+			target_vector.append(target_type)
+		clf = model
+		clf.fit(feature_vector, target_vector)
 
-	total_test = len(predict_apks)
-	number_correct = 0
-	print("predicting...")
-	for idx in predict_apks:
-			test_feature_vector = get_permissions(apks[idx])
-			result = clf.predict([test_feature_vector])
-			mal_status = 1 if apks[idx]['Malicious'] == 'true' else 0
-			if result == mal_status:
-					number_correct += 1
-	percent = (float(number_correct)/float(total_test))*float(100)
-	print("Model: SVC")
-	print("Accuracy: " + str(percent) + "%")
+		total_test = len(predict_apks)
+		number_correct = 0
+		print("predicting...")
+		for idx in predict_apks:
+				test_feature_vector = get_permissions(apks[idx])
+				result = clf.predict([test_feature_vector])
+				mal_status = 1 if apks[idx]['Malicious'] == 'true' else 0
+				if result == mal_status:
+						number_correct += 1
+		percent = (float(number_correct)/float(total_test))*float(100)
+		print("Model: " + model_name)
+		print("Accuracy: " + str(percent) + "%")
 
 main()
