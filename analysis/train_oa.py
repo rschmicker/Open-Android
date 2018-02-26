46cedcd5dd5cc66976534bb085c48ece7e14a194
Rob Schmicker <rhschmicker@gmail.com>
2018-02-26 13:29:49 -0500

    testing

--- analysis/train_oa.py
+++ analysis/train_oa.py
@@ -11,6 +11,7 @@ from sklearn import tree
 from sklearn import linear_model
 from sklearn.naive_bayes import GaussianNB
 from sklearn.cluster import KMeans
+from sklearn.model_selection import KFold
 
 # dl: 50 minutes for perms, filesize, and malicious for 94,000 apks, 12.9KB/s
 def get_apk_json(filepath):
@@ -27,32 +28,42 @@ def get_permissions(apk):
 	return perms
 
 def main():
-	model = linear_model.LogisticRegression()
-	#model = svm.SVC()
+	#model = linear_model.LogisticRegression()
+	model = svm.SVC()
 	#model = GaussianNB()
 	#model = KMeans(n_clusters = 10)
 	apks = get_apk_json("permsfilesize.json")
 	feature_vector = []
 	target_vector = []
 	apks = apks['data']
-	train_apks = apks[:len(apks)-10000]
-	predict_apks = apks[len(apks)-10000:]
-	for apk in train_apks:
-		feature_vector.append(get_permissions(apk))
-		target_type = 1 if apk['Malicious'] == 'true' else 0
+	kf = KFold(10, True, None)
+	kf.get_n_splits(apks)
+	train_apks = []
+	predict_apks = []
+	for train, test in kf.split(apks):
+		train_apks = train
+		predict_apks = test
+		break
+
+	print("training...")
+	for idx in train_apks:
+		feature_vector.append(get_permissions(apks[idx]))
+		target_type = 1 if apks[idx]['Malicious'] == 'true' else 0
 		target_vector.append(target_type)
 	clf = model
 	clf.fit(feature_vector, target_vector)
 
 	total_test = len(predict_apks)
 	number_correct = 0
-	for apk in predict_apks:
-			test_feature_vector = get_permissions(apk)
+	print("predicting...")
+	for idx in predict_apks:
+			test_feature_vector = get_permissions(apks[idx])
 			result = clf.predict([test_feature_vector])
-			mal_status = 1 if apk['Malicious'] == 'true' else 0
+			mal_status = 1 if apks[idx]['Malicious'] == 'true' else 0
 			if result == mal_status:
 					number_correct += 1
 	percent = (float(number_correct)/float(total_test))*float(100)
+	print("Model: SVC")
 	print("Accuracy: " + str(percent) + "%")
 
 main()
